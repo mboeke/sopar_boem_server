e06e014941f9a029ff2293314299ebb2bd021272
mboeke <maximilian.boeker@uzh.ch>
2019-03-07 20:56:10 +0100

    Fixed multiple @bean UserRepository & adjusted further variables to password

--- src/main/java/ch/uzh/ifi/seal/soprafs19/service/UserService.java
+++ src/main/java/ch/uzh/ifi/seal/soprafs19/service/UserService.java
@@ -19,8 +19,6 @@ public class UserService {
 
     private final UserRepository userRepository;
 
-
-    @Autowired
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
